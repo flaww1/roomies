e20364baeb9171afcfba91989520bd47614912dc
Flávio Pereira <flaviopereirayt@gmail.com>
2023-11-28 21:27:44 +0000

    test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 dependencyResolutionManagement {
@@ -10,8 +11,12 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
+
+
+
 
 rootProject.name = "Roomies"
 include(":app")
