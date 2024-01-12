7b80ab20e5b1679269b7f5decd16d7256d4b82df
guilongo1999 <95706403+guilongo1999@users.noreply.github.com>
2024-01-12 18:15:42 +0000

    clarificacao index.js

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.registerUser = functions.https.onRequest(async (req, res) => {
+exports.registerUser = functions.https.onRequest(async (req, res) => { //definido dois http func
   // Handle user registration logic here
   // Use admin.firestore() to interact with Firestore
   // Send response using res.send()
