ac5e4a612c0188ef7fc019a4e869957e1166d59b
guilongo1999 <95706403+guilongo1999@users.noreply.github.com>
2024-01-13 02:10:27 +0000

    Update MatchedUsersAdapter.kt

--- app/src/main/java/pt/ipca/roomies/ui/main/messages/MatchedUsersAdapter.kt
+++ app/src/main/java/pt/ipca/roomies/ui/main/messages/MatchedUsersAdapter.kt
@@ -10,26 +10,26 @@ import androidx.recyclerview.widget.RecyclerView
 import pt.ipca.roomies.R
 import pt.ipca.roomies.data.entities.Match
 
-class MatchedUsersAdapter(private val onItemClick: (Match) -> Unit) :
-    ListAdapter<Match, MatchedUsersAdapter.MatchedUserViewHolder>(MatchDiffCallback()) {
+class MatchedUsersAdapter(private val onItemClick: (Match) -> Unit) :   //lista de matches para a interface do utilizador
+    ListAdapter<Match, MatchedUsersAdapter.MatchedUserViewHolder>(MatchDiffCallback()) {  //  adaptador é usado para exibir uma lista de usuários correspondentes RecyclerView, e a classe MatchedUserViewHolder lida com a exibição e atualização dos itens individuais. click origina a acao apropriada
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MatchedUserViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MatchedUserViewHolder { //Preenche a visualização criada em onCreateViewHolder com os dados de um usuário correspondente específico. 
         val inflater = LayoutInflater.from(parent.context)
         val view = inflater.inflate(R.layout.item_matched_user, parent, false)
         return MatchedUserViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: MatchedUserViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MatchedUserViewHolder, position: Int) { //Informa ao adaptador qual lista de users correspondentes deve ser exibida.
         val matchedUser = getItem(position)
         holder.bind(matchedUser, onItemClick)
     }
 
-    fun submitList(matchedUsers: Set<Match>?) {
+    fun submitList(matchedUsers: Set<Match>?) { //informa adaptador qual lista de users correspondentes deve ser exibida. Quando a lista é atualizada, esta função é chamada para mostrar os novos users correspondentes.
         super.submitList(matchedUsers?.toList())
 
     }
 
-    class MatchedUserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class MatchedUserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {  /
         private val userNameTextView: TextView = itemView.findViewById(R.id.textViewMatchedUserName)
 
         fun bind(matchedUser: Match, onItemClick: (Match) -> Unit) {
@@ -41,7 +41,7 @@ class MatchedUsersAdapter(private val onItemClick: (Match) -> Unit) :
         }
     }
 
-    private class MatchDiffCallback : DiffUtil.ItemCallback<Match>() {
+    private class MatchDiffCallback : DiffUtil.ItemCallback<Match>() { //Ajuda a verificar quais itens mudaram na lista para que apenas as alterações necessárias sejam feitas.
         override fun areItemsTheSame(oldItem: Match, newItem: Match): Boolean {
             return oldItem.matchId == newItem.matchId
         }
